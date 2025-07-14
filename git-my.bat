a5fba34fe1dbf0d581a4e3fa6c4938359cf07902
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-14 18:45:31 +0800

    20250714 v3 Railway

--- git-my.bat
+++ git-my.bat
@@ -1,7 +1,7 @@
 cd "C:\Users\chbon\Documents\python\appwrite"
 git init
 git add .
-git commit -m "20250714 v2 Railway"
+git commit -m "20250714 v3 Railway"
 git remote add origin https://github.com/goldshoot0720/fastapiappwritegoldshoot0720.git
 git checkout -b master
 git push -u origin master --force
